9dacec531a5729198c954ccb32d03d4de4a3da01
DDStudent <njaramillo42@student.cnm.edu>
2026-03-24 11:56:53 -0600

    ahhhhhhhhhhhhh

--- MIdterm2Code/src/MIdterm2Code.cpp
+++ MIdterm2Code/src/MIdterm2Code.cpp
@@ -10,6 +10,7 @@
 #include "Particle.h"
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_GFX.h"
+#include "credentials.h"
 
 
 SYSTEM_MODE(AUTOMATIC);
@@ -18,8 +19,8 @@ String dateTime, timeOnly;
 unsigned int lastTime;
 const int OLED_RESET=-1;
 const int relay = D15;
-int reads;
-int pin = D16;
+int moistureLevel;
+int dust = D16;
 unsigned long duration;
 unsigned long starttime;
 unsigned long sampletime_ms = 30000;
@@ -27,9 +28,8 @@ unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
 float concentration = 0;
 const char *EVENT_NAME = "water";
-void subscriptionHandler(const char *event, const char *data);
+void waterTime(int moistureLevel, int relay);
 Adafruit_SSD1306 display(OLED_RESET);
-
 
 
 void setup() {
@@ -38,41 +38,77 @@ Time.zone(-6);
 Particle.syncTime();
 Serial.begin(9600);
 waitFor(Serial.isConnected,15000);
-pinMode(pin,INPUT);
+pinMode(dust,INPUT);
 starttime = millis();
-// String subscriptionName = String::format("%s/%s/", System.deviceID().c_str(), EVENT_NAME);
-// Particle.subscribe(subscriptionName, subscriptionHandler, MY_DEVICES);
-// Serial.printf("Subscribing to %s\n", subscriptionName.c_str());
+Time.zone(-6);
+Particle.syncTime();
+Serial.begin(9600);
+waitFor(Serial.isConnected,15000);
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+display.clearDisplay();
+
 }
 
 
 void loop() {
-reads = analogRead(A1);
-dateTime = Time.timeStr();
-timeOnly=dateTime.substring(11 ,19);
-if( millis () - lastTime >10000) {
+// autowater
+moistureLevel = analogRead(A1);
+waterTime(moistureLevel, relay);
+
+// display moisture level 
+if( millis() - lastTime >10000) {
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,20);
+display.printf("moisture\n %i\n ", moistureLevel);
+display.display();
 lastTime = millis();
-display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+delay(500);
+}
+
+// display dust
+duration = pulseIn(dust, LOW);
+lowpulseoccupancy = lowpulseoccupancy+duration;
+if ((millis()-starttime) > sampletime_ms){
+concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
 display.clearDisplay();
 display.setTextSize(2);
 display.setTextColor(WHITE);
 display.setCursor(0,25);
-display.printf("%i\n %s\n", reads, timeOnly.c_str());
+display.printf("dust\n %f ",concentration);
 display.display();
-delay(100);
-digitalWrite(relay,HIGH);
-delay(500);
-digitalWrite(relay, LOW);
-delay (2000);
+lowpulseoccupancy = 0;
+delay(2000);
+
+// display time
+dateTime = Time.timeStr();
+timeOnly=dateTime.substring(11 ,19);
+if ((millis()-starttime) > sampletime_ms){
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,25);
+display.printf("time\n %s\n",timeOnly.c_str());
+display.display();
+delay(2000);
+starttime = millis();
+// if (concentration >)
+}
+}
 }
 
-duration = pulseIn(pin, LOW);
-lowpulseoccupancy = lowpulseoccupancy+duration;
-if ((millis()-starttime) > sampletime_ms){
-ratio = lowpulseoccupancy/(sampletime_ms*10.0);
-concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
-Serial.printf("%li\n%f\n concentration %f\n",lowpulseoccupancy, ratio, concentration);
-lowpulseoccupancy = 0;
-starttime = millis();
+
+
+// auto water function
+void waterTime(int moistureLevel, int relay){
+static unsigned int longTime;
+if ((millis()-longTime) >6000){
+if (moistureLevel>2700){
+digitalWrite(relay, HIGH);
+delay(500);
+digitalWrite(relay, LOW);
+} 
+longTime = millis();
 }
 }
