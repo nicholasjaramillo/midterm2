85c4b88ce33354778b6d537a9f6786607daa74fe
DDStudent <njaramillo42@student.cnm.edu>
2026-03-25 08:06:10 -0600

    done

--- MIdterm2Code/src/MIdterm2Code.cpp
+++ MIdterm2Code/src/MIdterm2Code.cpp
@@ -11,7 +11,10 @@
 #include "Adafruit_SSD1306.h"
 #include "Adafruit_GFX.h"
 #include "credentials.h"
-
+#include "Adafruit_BME280.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include <Adafruit_MQTT.h>
 
 SYSTEM_MODE(AUTOMATIC);
 
@@ -19,7 +22,9 @@ String dateTime, timeOnly;
 unsigned int lastTime;
 const int OLED_RESET=-1;
 const int relay = D15;
-int moistureLevel;
+const int HEXADDRESS = 0x76;
+int moistureLevel = analogRead(A1);
+bool status;
 int dust = D16;
 unsigned long duration;
 unsigned long starttime;
@@ -27,13 +32,31 @@ unsigned long sampletime_ms = 30000;
 unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
 float concentration = 0;
+float tempC;
+float humidRH;
+void MQTT_connect ();
+bool MQTT_ping ();
+float  pubValue;
 const char *EVENT_NAME = "water";
 void waterTime(int moistureLevel, int relay);
+void grabTemp(float tempC,  float humidRH);
+void grabTime(String dateTime, String timeOnly);
+void grabDust(int dust, unsigned long starttime, unsigned long sampletime_ms, unsigned long duration, float concentration, unsigned long lowpulseoccupancy);
+void grabMoisture(int moistureLevel);
 Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_BME280 bme; 
+TCPClient TheClient; 
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+Adafruit_MQTT_Publish pubFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temp");
+Adafruit_MQTT_Publish pubFeedd = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/dust");
+Adafruit_MQTT_Publish pubFeeddd = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/moister");
 
 
 void setup() {
+Serial.begin (9600);
+waitFor( Serial.isConnected , 15000) ;
 pinMode (relay, OUTPUT);
+WiFi.connect(); 
 Time.zone(-6);
 Particle.syncTime();
 Serial.begin(9600);
@@ -46,28 +69,78 @@ Serial.begin(9600);
 waitFor(Serial.isConnected,15000);
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 display.clearDisplay();
+status = bme.begin(HEXADDRESS);
+if(status == false) {
+Serial.printf("BME280 at address 0x%02X failed to start ",HEXADDRESS);
+}
+}
 
+void loop() {
+waterTime(moistureLevel, relay);
+grabMoisture(moistureLevel);
+grabDust(dust, starttime, sampletime_ms, duration, concentration, lowpulseoccupancy);
+grabTime(dateTime, timeOnly);
+grabTemp(tempC, humidRH);
 }
 
 
-void loop() {
-// autowater
-moistureLevel = analogRead(A1);
-waterTime(moistureLevel, relay);
 
-// display moisture level 
-if( millis() - lastTime >10000) {
+// auto water function
+void waterTime(int moistureLevel, int relay){
+moistureLevel = analogRead(A1);
+static unsigned int longTime;
+if ((millis()-longTime) >6000){
+if (moistureLevel>2700){
+digitalWrite(relay, HIGH);
+delay(500);
+digitalWrite(relay, LOW);
+} 
+longTime = millis();
+}
+}
+
+// get temputure
+void grabTemp(float tempC,  float humidRH){
+tempC = bme.readTemperature();
+humidRH = bme.readHumidity();
+if( mqtt.Update() ){
+pubFeed.publish (tempC);
 display.clearDisplay();
 display.setTextSize(2);
 display.setTextColor(WHITE);
-display.setCursor(0,20);
-display.printf("moisture\n %i\n ", moistureLevel);
+display.setCursor(0,25);
+display.printf("temperture %f\n",tempC);
+display.clearDisplay();
+display.printf("humidity %f\n",humidRH);
 display.display();
-lastTime = millis();
-delay(500);
+delay(2000);
+}
 }
 
-// display dust
+// get time
+void grabTime(String dateTime, String timeOnly){
+dateTime = Time.timeStr();
+timeOnly=dateTime.substring(11 ,19);
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,25);
+display.printf("time\n %s\n",timeOnly.c_str());
+display.display();
+delay(2000);
+
+if(concentration > 0.7){
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,25);
+display.printf("!DUSTY!\n ");
+display.display();
+delay(2000);
+}
+}
+// get dust reading
+void grabDust(int dust, unsigned long starttime, unsigned long sampletime_ms, unsigned long duration, float concentration, unsigned long lowpulseoccupancy){
 duration = pulseIn(dust, LOW);
 lowpulseoccupancy = lowpulseoccupancy+duration;
 if ((millis()-starttime) > sampletime_ms){
@@ -79,36 +152,28 @@ display.setCursor(0,25);
 display.printf("dust\n %f ",concentration);
 display.display();
 lowpulseoccupancy = 0;
+pubFeedd.publish (concentration);
 delay(2000);
-
-// display time
-dateTime = Time.timeStr();
-timeOnly=dateTime.substring(11 ,19);
-if ((millis()-starttime) > sampletime_ms){
+}
+if(concentration > 0.7){
 display.clearDisplay();
 display.setTextSize(2);
 display.setTextColor(WHITE);
 display.setCursor(0,25);
-display.printf("time\n %s\n",timeOnly.c_str());
+display.printf("!DUSTY!\n ");
 display.display();
 delay(2000);
-starttime = millis();
-// if (concentration >)
 }
 }
-}
-
-
-
-// auto water function
-void waterTime(int moistureLevel, int relay){
-static unsigned int longTime;
-if ((millis()-longTime) >6000){
-if (moistureLevel>2700){
-digitalWrite(relay, HIGH);
+// display moisture
+void grabMoisture(int moistureLevel){
+moistureLevel = analogRead(A1);
+pubFeeddd.publish (moistureLevel);
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor(0,20);
+display.printf("moisture\n %i\n ", moistureLevel);
+display.display();
 delay(500);
-digitalWrite(relay, LOW);
-} 
-longTime = millis();
-}
 }
